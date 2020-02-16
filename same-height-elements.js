df1958c3563494eda42a7c10655df046debd8b8d
Donte <3503886+dthenley@users.noreply.github.com>
2020-02-15 21:07:05 -0600

    Basic Usage
    This is the base use of the same-height-element jquery code that makes the elements inside the container element all the same height

--- same-height-elements.js
+++ same-height-elements.js
@@ -1,9 +1,9 @@
-$('#parentElement').ready(function(){
+$('.element-container').ready(function(){
 	var max_height = 0;
-	$(this).find('.element').each( function(){
+	$(this).find('.single-element').each( function(){
 		max_height = max_height > $(this).height() ? max_height : $(this).height();
 	});
-	$(this).find('.element').each(function(){
+	$(this).find('.single-element').each(function(){
 		$(this).height(max_height);
 	})
 });

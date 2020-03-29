c90eb7b7fc68da98daaa576de10ce8cb5910db77
Donte <3503886+dthenley@users.noreply.github.com>
2020-03-29 14:39:10 -0500

    It Works!!!!!
    Able to use the same-height attribute to relate each other and make them the same height

--- same-height-elements.js
+++ same-height-elements.js
@@ -3,20 +3,35 @@ $( document ).ready(function() {
     $(window).resize(height_function);
 });
 
-
-
 function height_function(){
-$('.element-container').ready(function(){
-	var max_height = 0;
-	$(this).find('.single-element').each(function(){
-		$(this).height('auto');
-	})	
+	
+	// Set same_height_element to blank array
+	var same_height_element = [];
 
-	$(this).find('.single-element').each( function(){
-		max_height = max_height > $(this).height() ? max_height : $(this).height();
-	});
-	$(this).find('.single-element').each(function(){
-		$(this).height(max_height);
+	//Pushes all same-height attribute elements into same_height_element array
+	$('*[same-height]').each(function(){
+		same_height_element.push( $(this).attr('same-height') );
 	})
-});
+
+	//Strips out all unique elements from same_height_element into unique_same_height_element 
+	var unique_same_height_element = same_height_element.filter(function(itm, i, same_height_element) {
+		return i == same_height_element.indexOf(itm);
+	});
+
+	//Sets all same-height attribute elements to auto heihgt
+	$(this).height('auto');
+
+	var c = 0;
+	$.each(unique_same_height_element, function( index, value ) { 
+		var max_height = 0;
+		
+		//Loops through and compare the height of each element and saves the largest height into max_height
+		$('[same-height = '+ value +']').each( function(){
+			max_height = max_height > $(this).height() ? max_height : $(this).height();
+		});
+
+		//Makes height for all element into max_height
+		$('[same-height = '+ value +']').height(max_height);
+
+	  });
 }
